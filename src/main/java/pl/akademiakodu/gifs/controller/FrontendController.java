0d13ad78e39c5798905e6b958246ff68b12bbc4f
Michal Socha <michal.socha.88@gmail.com>
2020-02-16 11:23:10 +0100

    added frontend controller

---
+++ src/main/java/pl/akademiakodu/gifs/controller/FrontendController.java
@@ -0,0 +1,15 @@
+package pl.akademiakodu.gifs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FrontendController {
+
+    @ResponseBody
+    @GetMapping("/")
+    private String cheer() {
+        return "Hello";
+    }
+}
