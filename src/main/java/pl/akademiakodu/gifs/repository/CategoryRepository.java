ebc44e7ad082a1d24fba12339eed004605d95889
Michal Socha <michal.socha.88@gmail.com>
2020-02-16 15:30:18 +0100

    Added categories

---
+++ src/main/java/pl/akademiakodu/gifs/repository/CategoryRepository.java
@@ -0,0 +1,23 @@
+package pl.akademiakodu.gifs.repository;
+
+import org.springframework.stereotype.Repository;
+import pl.akademiakodu.gifs.model.Category;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class CategoryRepository {
+
+    private final List<Category> ALL_CATEGORY =Arrays.asList(
+
+            new Category(0, "funny"),
+            new Category(1, "sport"),
+            new Category(2, "it")
+    );
+
+
+    public List<Category> getCategories() {
+        return ALL_CATEGORY;
+    }
+}
